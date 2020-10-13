76461f30c26ffceaac6f53204e2576d96bac5289
Devesh Sharma <devesh.sharma@opstree.com>
2020-10-13 15:42:20 +0530

    Removed slack webhook

--- example/main.tf
+++ example/main.tf
@@ -13,7 +13,7 @@ module "petpark_eks_cluster" {
   source                 =  "../eks"
   cluster_name           =  var.cluster_name
   eks_cluster_version    =  "1.16"
-  subnets                =  ["subnet-057a78d8d14adf40c", "subnet-0ac0f6a4d0c41eecc"]
+  subnets                =  ["subnet-id_1", "subnet-id_2"]
   tags                   =  local.common_tags
   kubeconfig_name        =  "config"
   config_output_path     =  "config"
@@ -23,12 +23,12 @@ module "petpark_eks_cluster" {
   endpoint_public        =  true
   create_spot_node_group =  true
   create_node_group      =  false
-  vpc_id              =  "vpc-077a88f9398c12b39"
+  vpc_id              =  "vpc-xzd"
   slackUrl	          =  "slack_webhook URL"  
   node_groups = {}
  spot_node_group = {
 	"spot_worker1" = {
-	  subnets             = ["subnet-057a78d8d14adf40c", "subnet-0ac0f6a4d0c41eecc"]
+	  subnets             = ["subnet-1", "subnet-2"]
 	  instance_type       =   "m5a.2xlarge"
     ami_id              = "ami-xyz"
 	  disk_size           = 40
@@ -37,7 +37,7 @@ module "petpark_eks_cluster" {
     min_capacity        = 2
     ssh_key             = var.ssh_key
     spot_price          = "1.117"
-	  security_group_ids  = ["sg-0793542c5c6998159"]
+	  security_group_ids  = ["sg-xzy"]
 	  tags                = merge(local.common_tags, local.worker_group1_tags)
   	}
   }
